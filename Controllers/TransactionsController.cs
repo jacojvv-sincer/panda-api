1d2cd98bdde146179e2575de22369202fe84fcf5
Jaco Jansen van Vuuren <Jacojvv.hello@gmail.com>
2019-03-31 21:37:32 +0200

    Added addition of transactions & amended get

--- Controllers/TransactionsController.cs
+++ Controllers/TransactionsController.cs
@@ -26,39 +26,57 @@ namespace MoneyManagerApi.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Transaction>>> Get()
         {
-            return Ok(await _context.Transactions.Where(d => d.User.Id == _user.Id).OrderByDescending(d => d.Date).ToListAsync());
+            return Ok(await _context.Transactions
+                .Where(t => t.User.Id == _user.Id)
+                .OrderByDescending(t => t.Date)
+                .ThenByDescending(t => t.Id)
+                .Include(t => t.Category )
+                .Include(t => t.Location)
+                .Include(t => t.People)
+                .Include(t => t.Tags)
+                .GroupBy(t => t.Date)
+                .ToListAsync());
         }
 
-        // [HttpGet]
-        // [Route("latest")]
-        // public async Task<ActionResult<List<Demarcation>>> GetLatest()
-        // {
-        //     return Ok(await _context.Demarcations.Include(d => d.Buckets).ThenInclude(b => b.).Where(d => d.User.Id == _user.Id).OrderByDescending(d => d.StartDate).FirstAsync());
-        // }
+        [HttpPost]
+        public async Task<ActionResult<Transaction>> Post([FromBody] Transaction transaction)
+        {
+            if (!ModelState.IsValid)
+            {
+                return UnprocessableEntity(ModelState);
+            }
 
-        // [HttpPost]
-        // public async Task<ActionResult<Demarcation>> Post([FromBody] Demarcation demarcation)
-        // {
-        //     // todo fix end & start
-        //     if(!ModelState.IsValid){
-        //         return UnprocessableEntity(ModelState);
-        //     }
-        //     if(demarcation.StartDate >= demarcation.EndDate){
-        //         return UnprocessableEntity("The start date cannot be larger than the end date");
-        //     }
-        //     int startCount = await _context.Demarcations.Where(d => d.StartDate > demarcation.StartDate && d.EndDate < demarcation.StartDate).CountAsync();
-        //     if(startCount > 0){
-        //         return UnprocessableEntity("A demarcation already exists that covers the range of your start date");
-        //     }
-        //     int endCount = await _context.Demarcations.Where(d => d.EndDate > demarcation.StartDate && d.EndDate < demarcation.EndDate).CountAsync();
-        //     if(startCount > 0){
-        //         return UnprocessableEntity("A demarcation already exists that covers the range of your end date");
-        //     }
+            var newTransaction = new Transaction();
+            newTransaction.Amount = transaction.Category.Name == "Income" ? transaction.Amount : -transaction.Amount;
+            newTransaction.Date = transaction.Date;
+            newTransaction.IsExtraneous = transaction.IsExtraneous;
+            newTransaction.Label = transaction.Label;
+            newTransaction.Notes = transaction.Notes;
 
-        //     demarcation.User = _user;
-        //     _context.Demarcations.Add(demarcation);
-        //     await _context.SaveChangesAsync();
-        //     return Ok(demarcation);
-        // }
+            if (transaction.Category != null)
+            {
+                newTransaction.Category = await _context.Categories.FindAsync(transaction.Category.Id);
+            }
+            if (transaction.Location != null)
+            {
+                newTransaction.Location = await _context.Locations.FindAsync(transaction.Location.Id);
+            }
+            if (transaction.People != null)
+            {
+                var ids = transaction.People.Select(p => p.Id).ToArray();
+                newTransaction.People = await _context.People.Where(p => ids.Contains(p.Id)).ToListAsync();
+            }
+            if (transaction.Tags != null)
+            {
+                var ids = transaction.Tags.Select(t => t.Id).ToList();
+                newTransaction.Tags = await _context.Tags.Where(t => ids.Contains(t.Id)).ToListAsync();
+            }
+
+            newTransaction.User = _user;
+            _context.Transactions.Add(newTransaction);
+            await _context.SaveChangesAsync();
+            return Ok(newTransaction);
+        }
+
     }
 }
